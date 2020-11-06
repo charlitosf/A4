2aaf5c4014ffaac3018c7472c666029cd204e877
Carlos Ferrús Ferri <cmff1@alu.ua.es>
2020-11-06 23:37:47 +0100

    Testing moving to clock

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+clock: node index.js
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+//const express = require('express');
 require('dotenv').config();
 const axios = require('axios')
 
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
 const seconds = process.env.SECONDS || 0;
 const minutes = process.env.MINUTES || 1;
 const hours = process.env.HOURS || 0;
@@ -24,6 +24,7 @@ setInterval(() => {
     }
 }, interval);
 
-express()
+/*express()
     .get('/', (req, res) => res.send({interval: interval}))
     .listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+*/
