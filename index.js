63c315988c4609532146419412a8057a0b9effac
Carlos Ferrús Ferri <cmff1@alu.ua.es>
2020-11-06 21:57:34 +0100

    Little bugfix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const hours = process.env.HOURS || 0;
 const days = process.env.DAYS || 0;
 let interval;
 if (days <= 1)
-    interval = (days * 86400 + hours * 3600 + minutes * 60 + seconds) * 1000
+    interval = (days * 86400 + hours * 3600 + minutes * 60 + seconds * 1) * 1000
 else
     interval = (86400 + hours * 3600 + minutes * 60 + seconds) * 1000
 
